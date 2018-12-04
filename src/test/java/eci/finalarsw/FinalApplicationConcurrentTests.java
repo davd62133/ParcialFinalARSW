4e54e58dff3087ba733d2b91efb6ec8f305e9814
Rafael <rafael.jimenez@mail.escuelaing.edu.co>
2018-12-04 11:32:00 -0500

    Pruebas concurrentes

--- src/test/java/eci/finalarsw/FinalApplicationConcurrentTests.java
+++ src/test/java/eci/finalarsw/FinalApplicationConcurrentTests.java
@@ -1,10 +1,28 @@
 package eci.finalarsw;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FinalApplicationConcurrentTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void prueba1(){
+        System.out.println(
+                restTemplate.getForEntity("http://localhost:" + String.valueOf(port) + "/", String.class)
+
+        );
+
+    }
 }
